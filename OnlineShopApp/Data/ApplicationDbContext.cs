cd788e66e7b442406a0fd700d1e1d72b18ea4524
Nistor Gheorghe <nistorgeorge404@gmail.com>
2022-12-20 00:34:24 +0200

    resolve conflicts

--- OnlineShopApp/Data/ApplicationDbContext.cs
+++ OnlineShopApp/Data/ApplicationDbContext.cs
@@ -10,12 +10,7 @@ namespace OnlineShopApp.Data
             : base(options)
         {
         }
-<<<<<<< HEAD
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-=======
-        public DbSet<Models.Product> Products { get; set; }
-        public DbSet<Models.Category> Categories { get; set; }
->>>>>>> a372e4ea49dd9b2da2ce6f888fd0620544c752ef
     }
 }
--- OnlineShopApp/Models/Category.cs
+++ OnlineShopApp/Models/Category.cs
@@ -6,14 +6,9 @@ namespace OnlineShopApp.Models
     {
         [Key]
         public int Id { get; set; }
-<<<<<<< HEAD
-        public string? CategoryName { get; set; }
-=======
 
         [Required(ErrorMessage = "Numele categoriei este obligatoriu")]
         public string CategoryName { get; set; }
-
         public virtual ICollection<Product> Products { get; set; }
->>>>>>> a372e4ea49dd9b2da2ce6f888fd0620544c752ef
     }
 }
